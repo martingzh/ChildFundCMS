79e84f8304fb5f0a6b7b3a26047778df87bc1ee0
jim-krueger <jim.krueger@salesforce.com>
2021-05-20 11:44:00 -0400

    first commit - Jim

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,26 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.stereotype.*;
+
+@Controller
+
+@SpringBootApplication
+public class DemoApplication {
+       
+       @RequestMapping("/")
+       @ReqponseBody
+       String home() {
+         return "Hello World ChildFundCMS!";
+
+       } 
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
