3ccc139d764f940c928ac59d8b33f3ce64370b61
Matt Creaser <mcreaser@salesforce.com>
2021-06-04 12:13:53 -0300

    Read safety info from DB

--- src/main/java/org/childfund/models/Safety.java
+++ src/main/java/org/childfund/models/Safety.java
@@ -1,28 +1,45 @@
 package org.childfund.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.lang.Nullable;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Safety {
-  private final boolean lifeThreateningSituation;
-  @Nullable private final String situationDescription;
-  @Nullable private final String actionTaken;
+  @JsonProperty("group_health/Danger")
+  private String lifeThreateningSituation;
 
-  public Safety(
-      boolean lifeThreateningSituation,
-      @Nullable String situationDescription,
-      @Nullable String actionTaken) {
+  @Nullable
+  @JsonProperty("group_health/Danger_Situation")
+  private String situationDescription;
+
+  @Nullable
+  @JsonProperty("group_health/Danger_Situation_Remediation")
+  private String actionTaken;
+
+  public void setLifeThreateningSituation(String lifeThreateningSituation) {
     this.lifeThreateningSituation = lifeThreateningSituation;
-    this.situationDescription = situationDescription;
-    this.actionTaken = actionTaken;
   }
 
-  public boolean getLifeThreateningSituation() {
+  public String getLifeThreateningSituation() {
     return lifeThreateningSituation;
+  }
+
+  public boolean isLifeThreatened() {
+    return lifeThreateningSituation.equalsIgnoreCase("yes");
+  }
+
+  public void setSituationDescription(String situationDescription) {
+    this.situationDescription = situationDescription;
   }
 
   @Nullable
   public String getSituationDescription() {
     return situationDescription;
+  }
+
+  public void setActionTaken(String actionTaken) {
+    this.actionTaken = actionTaken;
   }
 
   @Nullable
