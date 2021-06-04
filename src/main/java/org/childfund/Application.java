2d1fc8df3a88fed1750aa750e404e6cd697f58e4
jim-krueger <jim.krueger@salesforce.com>
2021-06-04 11:17:57 -0400

    Create new login form [create-login]

--- src/main/java/org/childfund/Application.java
+++ src/main/java/org/childfund/Application.java
@@ -4,7 +4,10 @@ import org.childfund.config.CFConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
@@ -15,6 +18,13 @@ public class Application {
   @ResponseBody
   String home() {
     return "Hello World ChildFundCMS!";
+  }
+
+  @GetMapping("/login")
+  public String login(@RequestParam(name="name", required=false, defaultValue="World") String name, Model model) {
+    model.addAttribute("name", name);
+    return "login";
+
   }
 
   public static void main(String[] args) {
