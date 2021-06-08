c48d64ba04bec28ffea5433dd5850bf3562217e8
Jatin Taneja <jtaneja@jtaneja-ltm1qtz.internal.salesforce.com>
2021-06-07 22:41:53 -0700

    childhomePage

---
+++ src/main/java/org/childfund/controller/ChildHomeController.java
@@ -0,0 +1,25 @@
+package org.childfund.controller;
+
+import java.util.List;
+import org.childfund.models.Child;
+import org.childfund.service.ImmediateAttentionChildren;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChildHomeController {
+
+  private static final int NUMBER_OF_RECENT_DAYS_TO_CHECK = 50;
+
+  @Autowired private ImmediateAttentionChildren immediateAttentionChildren;
+
+  @GetMapping("/childHome")
+  public String retrieveChildrenList(Model model) {
+    List<Child> children =
+        immediateAttentionChildren.getRecentSurveyedChildren(NUMBER_OF_RECENT_DAYS_TO_CHECK);
+    model.addAttribute("children", children);
+    return "childHome";
+  }
+}
